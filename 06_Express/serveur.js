b50ae4215052f0d6179a919633f2887d90e19321
khayati <oualid.khayati@gmail.com>
2020-11-13 11:08:19 +0100

    exemple d'implantation de middlewares

--- 06_Express/serveur.js
+++ 06_Express/serveur.js
@@ -1,5 +1,23 @@
+const bodyparser = require('body-parser')
 const express = require('express')
+
+
+const mid1 = function (req, res, next) {
+    console.log('mid1 traitement 1');
+    next();
+    console.log('mid1 traitement 2');
+};
+const mid2 = function (req, res, next) {
+    console.log('mid2 traitement 1');
+    next();
+    console.log('mid2 traitement 2');
+};
+
 const app = express()
+app.use(bodyparser.json())
+app.use(mid1)
+app.use(mid2)
+
 const port = 3000
 
 app.get('/', (req, res) => {
@@ -20,10 +38,17 @@ app.get('/addition', (req, res) => {
     msg+='y='+y+"<br>"
     msg+='x+y='+(x+y)
     res.send(msg)
-    
-
   })
 
+  app.post('/multiplication', (req, res) => {
+    let x=parseFloat(req.body.x)
+    let y=parseFloat(req.body.y)
+    let msg='nous allons calculer une addition.<br>'
+    msg+='x='+x+"<br>"
+    msg+='y='+y+"<br>"
+    msg+='x+y='+(x+y)
+    res.send(msg)
+  })
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
