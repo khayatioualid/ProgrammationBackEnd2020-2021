baa47fb95329b247783a41620b3bbc670464218c
khayati <oualid.khayati@gmail.com>
2020-11-13 10:40:23 +0100

    ajout d'un webservice get avec des parametres

--- 06_Express/serveur.js
+++ 06_Express/serveur.js
@@ -6,6 +6,24 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/addition', (req, res) => {
+    let x=parseFloat(req.query.x)
+    let y=parseFloat(req.query.y)
+    /*
+    res.write('nous allons calculer une addition.\n')
+    res.write('x='+x+"\n")
+    res.write('y='+y+"\n")
+    res.end('x+y='+(x+y))
+    */
+    let msg='nous allons calculer une addition.<br>'
+    msg+='x='+x+"<br>"
+    msg+='y='+y+"<br>"
+    msg+='x+y='+(x+y)
+    res.send(msg)
+    
+
+  })
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
